13670436042b002589519d7c82ece7fae04a3aa6
Oleksiy Malynovskyi <malynovskyi123@gmail.com>
2026-07-03 18:10:33 +0300

    feat(domain): add Player entity and PlayerRegion enum

---
+++ Domain/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MatchmakingEngine.Domain;
+
+public class Player
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    [Required]
+    [MaxLength(50)] 
+    public string Username { get; set; } = string.Empty;
+
+    [Range(0, 5000)]
+    public double Mmr { get; set; } = 1000;
+
+    [Range(0.0, 1.0)]
+    public double TrustFactor { get; set; } = 0.5;
+
+    public PlayerRegion Region { get; set; } = PlayerRegion.EuWest;
+
+    public DateTimeOffset CreatedAt { get; init; } = DateTimeOffset.UtcNow;
+}

f3b361ff665e6ddfa855aae87697cc87788256e2
Oleksiy Malynovskyi <malynovskyi123@gmail.com>
2026-07-04 11:58:53 +0300

    feat(api): implement player registration endpoint and DTO

---
+++ Controllers/PlayersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MatchmakingEngine.Data;
+using MatchmakingEngine.DTO;
+using MatchmakingEngine.Domain;
+
+namespace MatchmakingEngine.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PlayersController : ControllerBase
+{
+    private readonly MatchmakingDbContext _context;
+    public PlayersController(MatchmakingDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register([FromBody] RegisterPlayerRequest request)
+    {
+        var player = new Player
+        {
+            Username = request.Username,
+            Region = request.Region
+        };
+
+        _context.Players.Add(player);
+
+        await _context.SaveChangesAsync();
+
+        return StatusCode(201, player);
+    }
+
+}
---
+++ DTO/RegisterPlayerRequest.cs
@@ -0,0 +1,9 @@
+﻿using MatchmakingEngine.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace MatchmakingEngine.DTO;
+
+public record RegisterPlayerRequest(
+    [Required][MaxLength(50)] string Username,
+    PlayerRegion Region
+);

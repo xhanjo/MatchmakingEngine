e13a38fce98deaad0bd947c1feca6fd5fbafe6fd
Oleksiy Malynovskyi <malynovskyi123@gmail.com>
2026-07-05 11:33:27 +0300

    feat(matchmaking): implement in-memory MatchmakingQueue service

---
+++ Services/MatchmakingQueue.cs
@@ -0,0 +1,23 @@
+﻿using MatchmakingEngine.Domain;
+using System.Threading.Channels;
+
+namespace MatchmakingEngine.Services;
+
+public class MatchmakingQueue : IMatchmakingQueue
+{
+    private readonly Channel<MatchmakingTicket> _queue;
+
+    public MatchmakingQueue()
+    {
+        _queue = Channel.CreateUnbounded<MatchmakingTicket>();
+    }
+    public async ValueTask<MatchmakingTicket?> DequeueAsync(CancellationToken cancellationToken)
+    {
+        return await _queue.Reader.ReadAsync(cancellationToken);
+    }
+
+    public async ValueTask EnqueueAsync(MatchmakingTicket ticket)
+    {
+        await _queue.Writer.WriteAsync(ticket);
+    }
+}

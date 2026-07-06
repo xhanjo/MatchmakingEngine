ee881e7a84ce2fd4c910e82c4acab8c242e46604
Oleksiy Malynovskyi <malynovskyi123@gmail.com>
2026-07-06 11:57:13 +0300

    feat(engine): implement MMR/trust matching algorithm and graceful shutdown

--- Services/MatchmakingWorker.cs
+++ Services/MatchmakingWorker.cs
@@ -1,9 +1,17 @@
-﻿namespace MatchmakingEngine.Services;
+﻿using MatchmakingEngine.Domain;
+using System.Transactions;
+
+namespace MatchmakingEngine.Services;
 
 public class MatchmakingWorker : BackgroundService
 {
     private readonly IMatchmakingQueue _queue;
     private readonly ILogger<MatchmakingWorker> _logger;
+
+    private readonly List<MatchmakingTicket> _waitingRoom = new();
+
+    private const double MaxMmrDifference = 100.0;
+    private const double MaxTrustDifference = 0.3;
     public MatchmakingWorker(IMatchmakingQueue queue, ILogger<MatchmakingWorker> logger)
     {
         _queue = queue;
@@ -13,16 +21,50 @@ public class MatchmakingWorker : BackgroundService
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         _logger.LogInformation("[Engine] async Matchmaking worker initialized.");
-        
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            var ticket = await _queue.DequeueAsync(stoppingToken);
 
-            if (ticket != null)
+        try
+        {
+            while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("[Queue] Ticket dequeued for player {Username} (MMR: {Mmr}, Region: {Region}). Ready for match evaluation!",
-                    ticket.Username, ticket.Mmr, ticket.Region);
+                var newTicket = await _queue.DequeueAsync(stoppingToken);
+
+                if (newTicket != null)
+                {
+                    _logger.LogInformation("[Queue] Ticket dequeued for player {Username} (MMR: {Mmr}, Region: {Region}). Ready for match evaluation!",
+                        newTicket.Username, newTicket.Mmr, newTicket.Region);
+
+                    TryMatchPlayer(newTicket);
+                }
             }
+        }
+        catch (OperationCanceledException)
+        {
+            _logger.LogInformation("[Engine] Matchmaking worker is shutting down gracefully.");
+        }
+
+    }
+
+    private void TryMatchPlayer(MatchmakingTicket newTicket)
+    {
+        var opponent = _waitingRoom.FirstOrDefault(waitingTicket =>
+        waitingTicket.Region == newTicket.Region &&
+        Math.Abs(waitingTicket.Mmr - newTicket.Mmr) <= MaxMmrDifference &&
+        Math.Abs(waitingTicket.TrustFactor - newTicket.TrustFactor) <= MaxTrustDifference);
+
+        if (opponent != null)
+        {
+            _waitingRoom.Remove(opponent);
+
+            var lobbyId = Guid.NewGuid();
+
+            _logger.LogWarning("[MATCH FOUND] Lobby {LobbyId} created! Player [{P1}] (MMR: {M1}) vs Player [{P2}] (MMR: {M2}) on Region {Region}!",
+                lobbyId, newTicket.Username, newTicket.Mmr, opponent.Username, opponent.Mmr, newTicket.Region);
+        }
+        else
+        {
+            _waitingRoom.Add(newTicket);
+            _logger.LogInformation("[Waiting Room] No match found for {Username}. Added to waiting room. (Total waiting: {Count}",
+                newTicket.Username, _waitingRoom.Count);
         }
     }
 }

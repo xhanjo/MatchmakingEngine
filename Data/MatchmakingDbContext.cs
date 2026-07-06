32e627eb7d4db19da21371e64439c8afceb66aa3
Oleksiy Malynovskyi <malynovskyi123@gmail.com>
2026-07-06 12:01:49 +0300

    feat(domain): add Match record and update DbContext

--- Data/MatchmakingDbContext.cs
+++ Data/MatchmakingDbContext.cs
@@ -6,6 +6,8 @@ namespace MatchmakingEngine.Data;
 public class MatchmakingDbContext : DbContext
 {
     public DbSet<Player> Players { get; set; }
+    public DbSet<Match> Matches { get; set; }
+
     public MatchmakingDbContext(DbContextOptions<MatchmakingDbContext> options) : base(options)
     {        
     }
---
+++ Domain/Match.cs
@@ -0,0 +1,9 @@
+﻿namespace MatchmakingEngine.Domain;
+
+public record Match(
+    Guid Id,
+    Guid Player1Id,
+    Guid Player2Id,
+    double AverageMmr,
+    DateTimeOffset CreatedAt
+    );

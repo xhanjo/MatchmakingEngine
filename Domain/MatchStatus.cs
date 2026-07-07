1ce6874e579161005e751669db4942869149875f
Oleksiy Malynovskyi <malynovskyi123@gmail.com>
2026-07-07 12:29:42 +0300

    feat(domain): add Finished status to MatchStatus enum

--- Domain/MatchStatus.cs
+++ Domain/MatchStatus.cs
@@ -4,5 +4,6 @@ public enum MatchStatus
 {
     Pending, // Waiting for all players to accept match
     Accepted, // All players accept match
-    Canceled // Somebody reject or don't accept match
+    Canceled, // Somebody reject or don't accept match
+    Finished // Completed match
 }

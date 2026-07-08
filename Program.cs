7d0f8e73b276de7a2a5228f3228fdf094319ebe9
Oleksiy Malynovskyi <malynovskyi123@gmail.com>
2026-07-08 10:37:54 +0300

    feat(services): implement global exception handler and register in pipeline

--- Program.cs
+++ Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddDbContext<MatchmakingDbContext>(options =>
 );
 
 var app = builder.Build();
+app.UseExceptionHandler();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ Services/GlobalExceptionHandler.cs
@@ -0,0 +1,44 @@
+﻿using MatchmakingEngine.Domain.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchmakingEngine.Services;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandler> _logger;
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception, "Exception occured: {Message}", exception.Message);
+
+        var (statusCode, title) = exception switch
+        {
+            NotFoundException => (StatusCodes.Status404NotFound, "Resource Not Found"),
+            ConflictException => (StatusCodes.Status409Conflict, "State Conflict"),
+            ValidationException => (StatusCodes.Status400BadRequest, "Validation Error"),
+            _ => (StatusCodes.Status500InternalServerError, "Internal Server Error")
+        };
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = statusCode,
+            Title = title,
+            Detail = exception.Message,
+            Instance = httpContext.Request.Path
+        };
+
+        httpContext.Response.StatusCode = statusCode;
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}

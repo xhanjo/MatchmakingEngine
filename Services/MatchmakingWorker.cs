64f424dd096b415301694353c56cd727da1057f4
Oleksiy Malynovskyi <malynovskyi123@gmail.com>
2026-07-10 13:09:09 +0300

    feat(matchmaking): implement smart expanding mmr brackets and remove in-memory queue

--- Services/MatchmakingWorker.cs
+++ Services/MatchmakingWorker.cs
@@ -1,20 +1,17 @@
 ﻿using MatchmakingEngine.Domain;
-using System.Transactions;
-using Microsoft.Extensions.DependencyInjection;
 using MatchmakingEngine.Data;
 using Microsoft.AspNetCore.SignalR;
 using MatchmakingEngine.Hubs;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace MatchmakingEngine.Services;
 
 public class MatchmakingWorker : BackgroundService
 {
     private readonly IMatchmakingQueue _queue;
-    private readonly ILogger<MatchmakingWorker> _logger;
     private readonly IServiceScopeFactory _scopeFactory;
+    private readonly ILogger<MatchmakingWorker> _logger;
     private readonly IHubContext<MatchmakingHub> _hubContext;
-
-    private readonly List<MatchmakingTicket> _waitingRoom = new();
 
     private const double MaxMmrDifference = 100.0;
     private const double MaxTrustDifference = 0.3;
@@ -22,92 +19,111 @@ public class MatchmakingWorker : BackgroundService
         IMatchmakingQueue queue,
         ILogger<MatchmakingWorker> logger,
         IServiceScopeFactory scopeFactory,
-        IHubContext<MatchmakingHub> hubContext
-        )
+        IHubContext<MatchmakingHub> hubContext)
     {
         _queue = queue;
-        _logger = logger;
         _scopeFactory = scopeFactory;
+        _logger = logger;
         _hubContext = hubContext;
     }
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _logger.LogInformation("[Engine] async Matchmaking worker initialized.");
+        _logger.LogInformation("[Engine] Smart Matchmaking worker with Redis ZSET initialized.");
 
-        try
+
+        while (!stoppingToken.IsCancellationRequested)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            try
             {
-                var newTicket = await _queue.DequeueAsync(stoppingToken);
+                var anchorId = await _queue.DequeueEvaluationIdAsync(stoppingToken);
 
-                if (newTicket != null)
-                {
-                    _logger.LogInformation("[Queue] Ticket dequeued for player {Username} (MMR: {Mmr}, Region: {Region}). Ready for match evaluation!",
-                        newTicket.Username, newTicket.Mmr, newTicket.Region);
+                if (anchorId.HasValue)
+                    await ProcessEvaluationAsync(anchorId.Value, stoppingToken);
+            }
 
-                    await TryMatchPlayerAsync(newTicket);
-                }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occured during matchmaking cycle.");
+                await Task.Delay(2000, stoppingToken);
             }
         }
-        catch (OperationCanceledException)
-        {
-            _logger.LogInformation("[Engine] Matchmaking worker is shutting down gracefully.");
-        }
-
     }
 
-    private async Task TryMatchPlayerAsync(MatchmakingTicket newTicket)
+
+    private async Task ProcessEvaluationAsync(Guid anchorId, CancellationToken cancellationToken)
     {
-        var opponent = _waitingRoom.FirstOrDefault(waitingTicket =>
-        waitingTicket.Region == newTicket.Region &&
-        Math.Abs(waitingTicket.Mmr - newTicket.Mmr) <= MaxMmrDifference &&
-        Math.Abs(waitingTicket.TrustFactor - newTicket.TrustFactor) <= MaxTrustDifference);
+        var anchor = await _queue.GetTicketAsync(anchorId);
+        if (anchor == null) return;
+
+        var waitTimeSeconds = DateTimeOffset.UtcNow - anchor.EnqueuedAt;
+
+        var currentDelta = Math.Min(300, 50 + waitTimeSeconds.TotalSeconds * 5);
+
+        var minMmr = anchor.Mmr - currentDelta;
+        var maxMmr = anchor.Mmr + currentDelta;
+
+        var candidateIds = await _queue.GetCandidatesByMmrRangeAsync(anchor.Region, minMmr, maxMmr);
+
+        MatchmakingTicket? opponent = null;
+
+        foreach (var candidate in candidateIds)
+        {
+            if (candidate == anchor.PlayerId) continue;
+
+            var candidateTicket = await _queue.GetTicketAsync(candidate);
+            if (candidateTicket == null) continue;
+
+            var trustDiff = Math.Abs(anchor.TrustFactor - candidateTicket.TrustFactor);
+
+            if (trustDiff <= 0.3)
+            {
+                opponent = candidateTicket;
+                break;
+            }
+        }
 
         if (opponent != null)
         {
-            _waitingRoom.Remove(opponent);
-            _queue.RemovePlayer(newTicket.PlayerId);
-            _queue.RemovePlayer(opponent.PlayerId);
-
-            var lobbyId = Guid.NewGuid();
-            var match = new Match(
-                Id: lobbyId,
-                Player1Id: newTicket.PlayerId,
-                Player2Id: opponent.PlayerId,
-                AverageMmr: (newTicket.Mmr + opponent.Mmr) / 2.0,
-                CreatedAt: DateTimeOffset.UtcNow
-                );
-
-            // Dbcontext - scoper, worker singletone => scopefactory for temporary scope
-            using (var scope = _scopeFactory.CreateScope())
-            {
-                var dbContext = scope.ServiceProvider.GetRequiredService<MatchmakingDbContext>();
-
-                dbContext.Matches.Add(match);
-                await dbContext.SaveChangesAsync();
-            }
-
-            var matchPayload = new
-            {
-                LobbyId = lobbyId,
-                AverageMmr = match.AverageMmr,
-                Message = "Match found! Please accept the match."
-            };
-
-            await _hubContext.Clients.Group(newTicket.PlayerId.ToString())
-                .SendAsync("MatchFound", matchPayload);
-
-            await _hubContext.Clients.Group(opponent.PlayerId.ToString())
-                .SendAsync("MatchFound", matchPayload);
-
-            _logger.LogWarning("[MATCH FOUND] Lobby {LobbyId} created! Player [{P1}] (MMR: {M1}) vs Player [{P2}] (MMR: {M2}) on Region {Region}!",
-                lobbyId, newTicket.Username, newTicket.Mmr, opponent.Username, opponent.Mmr, newTicket.Region);
+            await CreateMatchAndNotifyAsync(anchor, opponent);
         }
         else
         {
-            _waitingRoom.Add(newTicket);
-            _logger.LogInformation("[Waiting Room] No match found for {Username}. Added to waiting room. (Total waiting: {Count}",
-                newTicket.Username, _waitingRoom.Count);
+            await _queue.EnqueueAsync(anchor);
+
+            _logger.LogInformation("[Queue] {User} waiting {Wait:N0}s. Delta {Delta} Requeued.",
+               anchor.Username, waitTimeSeconds, currentDelta);
+            }
+
+    }
+
+
+    private async Task CreateMatchAndNotifyAsync(MatchmakingTicket p1, MatchmakingTicket p2)
+    {
+        _queue.RemovePlayer(p1);
+        _queue.RemovePlayer(p2);
+
+        var lobbyId = Guid.NewGuid();
+        var match = new Match(
+            Id: lobbyId,
+            Player1Id: p1.PlayerId,
+            Player2Id: p2.PlayerId,
+            AverageMmr: (p1.Mmr + p2.Mmr) / 2.0,
+            CreatedAt: DateTimeOffset.UtcNow
+            );
+
+        using (var scope = _scopeFactory.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<MatchmakingDbContext>();
+            dbContext.Matches.Add(match);
+            await dbContext.SaveChangesAsync();
         }
+
+        var matchPayload = new { LobbyId = lobbyId, AverageMmr = match.AverageMmr };
+
+        await _hubContext.Clients.Group(p1.PlayerId.ToString()).SendAsync("MatchFound", matchPayload);
+        await _hubContext.Clients.Group(p2.PlayerId.ToString()).SendAsync("MatchFound", matchPayload);
+
+        _logger.LogWarning("[MATCH FOUND] Lobby {LobbyId}! [{P1}] vs [{P2}] on {Region}!",
+            lobbyId, p1.Username, p2.Username, p1.Region);
     }
 }

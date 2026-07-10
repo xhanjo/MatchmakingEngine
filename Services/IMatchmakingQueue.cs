781635a3f48ff38c7eb10682caf1235f900bf28c
Oleksiy Malynovskyi <malynovskyi123@gmail.com>
2026-07-10 12:33:09 +0300

    refactor(redis): prepare queue structures for dynamic mmr brackets

--- Services/IMatchmakingQueue.cs
+++ Services/IMatchmakingQueue.cs
@@ -5,8 +5,12 @@ namespace MatchmakingEngine.Services;
 public interface IMatchmakingQueue
 {
     ValueTask EnqueueAsync(MatchmakingTicket ticket);
-    ValueTask<MatchmakingTicket?> DequeueAsync(CancellationToken cancellationToken);
+    ValueTask<Guid?> DequeueEvaluationIdAsync(CancellationToken cancellationToken = default);
+
+    ValueTask<MatchmakingTicket?> GetTicketAsync(Guid playerId);
+
+    ValueTask<Guid[]> GetCandidatesByMmrRangeAsync(PlayerRegion region, double minMmr, double maxMmr);
 
     bool IsPlayerInQueue(Guid playerId);
-    void RemovePlayer(Guid playerId);
+    void RemovePlayer(MatchmakingTicket ticket);
 }
--- Services/MatchmakingQueue.cs
+++ Services/MatchmakingQueue.cs
@@ -1,7 +1,5 @@
 ﻿using MatchmakingEngine.Domain;
-using Microsoft.AspNetCore.Mvc.Routing;
 using StackExchange.Redis;
-using System.Collections;
 using System.Text.Json;
 
 namespace MatchmakingEngine.Services;
@@ -9,30 +7,56 @@ namespace MatchmakingEngine.Services;
 public class MatchmakingQueue : IMatchmakingQueue
 {
     private readonly IDatabase _redisDb;
-    private const string QueueKey = "matchmaking_queue";
+    private const string EvaluationQueueKey = "matchmaking_queue";
     private const string ActivePlayersHashKey = "active_players";
 
     public MatchmakingQueue(IConnectionMultiplexer redisConnection)
     {
         _redisDb = redisConnection.GetDatabase();
     }
+
     public async ValueTask EnqueueAsync(MatchmakingTicket ticket)
     {
+        var playerIdStr = ticket.PlayerId.ToString();
         var json = JsonSerializer.Serialize(ticket);
-        await _redisDb.ListRightPushAsync(QueueKey, json);
-        await _redisDb.HashSetAsync(ActivePlayersHashKey, ticket.PlayerId.ToString(), json);
+
+        await _redisDb.HashSetAsync(ActivePlayersHashKey, playerIdStr, json);
+
+        await _redisDb.ListRightPushAsync(EvaluationQueueKey, playerIdStr);
+
+        await _redisDb.SortedSetAddAsync($"mmr_index:{ticket.Region}", playerIdStr, ticket.Mmr);
     }
 
-    public async ValueTask<MatchmakingTicket?> DequeueAsync(CancellationToken cancellationToken = default)
+    public async ValueTask<Guid?> DequeueEvaluationIdAsync(CancellationToken cancellationToken = default)
     {
-        var redisValue = await _redisDb.ListLeftPopAsync(QueueKey);
+        var redisValue = await _redisDb.ListLeftPopAsync(EvaluationQueueKey);
 
-        if (redisValue.HasValue == false)
+        if (!redisValue.HasValue)
         {
             await Task.Delay(1000, cancellationToken);
             return null;
         }
-        return JsonSerializer.Deserialize<MatchmakingTicket>(redisValue.ToString());
+
+        return Guid.Parse(redisValue.ToString());
+    }
+
+    public async ValueTask<MatchmakingTicket?> GetTicketAsync(Guid playerId)
+    {
+        var json = await _redisDb.HashGetAsync(ActivePlayersHashKey, playerId.ToString());
+
+        if (!json.HasValue)
+            return null;
+
+        return JsonSerializer.Deserialize<MatchmakingTicket>(json.ToString());
+    }
+
+    public async ValueTask<Guid[]> GetCandidatesByMmrRangeAsync(PlayerRegion region,double minMmr, double maxMmr )
+    {
+        var indexKey = $"mmr_index:{region}";
+
+        var values = await _redisDb.SortedSetRangeByScoreAsync(indexKey, start: minMmr, stop: maxMmr);
+
+        return values.Select(v => Guid.Parse(v.ToString())).ToArray();
     }
 
     public bool IsPlayerInQueue(Guid playerId)
@@ -40,8 +64,12 @@ public class MatchmakingQueue : IMatchmakingQueue
         return _redisDb.HashExists(ActivePlayersHashKey, playerId.ToString());
     }
 
-    public void RemovePlayer(Guid playerId)
+    public void RemovePlayer(MatchmakingTicket ticket)
     {
-        _redisDb.HashDelete(ActivePlayersHashKey, playerId.ToString());
+        var playerIdStr = ticket.PlayerId.ToString();
+        var indexKey = $"mmr_index:{ticket.Region}";
+
+        _redisDb.HashDelete(ActivePlayersHashKey, playerIdStr);
+        _redisDb.SortedSetRemove(indexKey, playerIdStr);
     }
 }

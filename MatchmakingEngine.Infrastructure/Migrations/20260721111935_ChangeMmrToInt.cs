feff33bc90f6e6167089c1335494194fec75409c
Oleksiy Malynovskyi <malynovskyi123@gmail.com>
2026-07-21 14:49:45 +0300

    chore(ef-core): add ChangeMmrToInt migration

---
+++ MatchmakingEngine.Infrastructure/Migrations/20260721111935_ChangeMmrToInt.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchmakingEngine.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeMmrToInt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Mmr",
+                table: "Players",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AverageMmr",
+                table: "Matches",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Mmr",
+                table: "Players",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "AverageMmr",
+                table: "Matches",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}

0e714dad1e4de126d8234a22dbb8e89161bdcd03
Oleksiy Malynovskyi <malynovskyi123@gmail.com>
2026-08-10 11:23:08 +0300

    chore: remove accidental test.cs file

--- test.cs
+++
@@ -1,1 +0,0 @@
-using System; class Program { static void Main() { var mode = Ranked5v5; Console.WriteLine(${{matchmaking_queue_{mode}}}); Console.WriteLine(${{matchmaking_queue_{mode}}}_processing); } }
